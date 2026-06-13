e66c7a92b97b68614404089ffe6e81e190c6d5c4
ahmernadeem306-debug <ahmernadeem306@gmail.com>
2026-06-13 22:30:42 +0500

    Add RAG-Powered caption to app
    Added RAG-Powered caption for enhanced user guidance.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.embeddings import HuggingFaceEmbeddings
